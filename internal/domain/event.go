4b30bd0c46154a496aef2e6dd160038d846dcacd
Luis Miguel <2001lmbl@gmail.com>
2026-01-22 09:30:27 -0300

    feat: domain event metadata

--- internal/domain/event.go
+++ internal/domain/event.go
@@ -8,23 +8,32 @@ import (
 
 type EventName string
 type EventID string
+type EventMetadata struct {
+}
 
 type Event interface {
 	EventName() EventName
 	EventID() EventID
 	OccurredAt() time.Time
+	Metadata() EventMetadata
 }
 
 type EventBase struct {
 	eventID    EventID
 	occurredAt time.Time
+	metadata   EventMetadata
 }
 
 func NewEventBase() *EventBase {
 	return &EventBase{
 		eventID:    EventID(util.NewUUID()),
 		occurredAt: time.Now(),
+		metadata:   EventMetadata{},
 	}
+}
+
+func (e *EventBase) WithMetadata(metadata EventMetadata) {
+	e.metadata = metadata
 }
 
 func (e *EventBase) EventID() EventID {
@@ -32,4 +41,7 @@ func (e *EventBase) EventID() EventID {
 }
 func (e *EventBase) OccurredAt() time.Time {
 	return e.occurredAt
+}
+func (e *EventBase) Metadata() EventMetadata {
+	return e.metadata
 }
